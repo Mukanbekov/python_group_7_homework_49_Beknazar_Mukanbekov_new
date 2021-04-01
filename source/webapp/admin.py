38ff9b56344c0670b1e1ee6d4577272733acebbb
MB7 <beknazarmb@mail.ru>
2021-04-01 17:17:59 +0600

    Создал админку

--- source/webapp/admin.py
+++ source/webapp/admin.py
@@ -1,9 +1,18 @@
 from django.contrib import admin
 
 # Register your models here.
-from webapp.models import List, Status, Type
+from webapp.models import List, Status, Type, Project
 
 
-admin.site.register(List)
+class ListAdmin(admin.ModelAdmin):
+    list_display = ['id', 'name', 'created_at', 'updated_at']
+    list_filter = ['name']
+    search_fields = ['name']
+    fields = ['id', 'name', 'created_at', 'updated_at']
+    readonly_fields = ['id', 'created_at', 'updated_at']
+
+
+admin.site.register(List, ListAdmin)
 admin.site.register(Status)
 admin.site.register(Type)
+admin.site.register(Project)
