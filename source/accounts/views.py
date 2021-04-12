14216b7c384225531ef44c9f9b0c66a9d648d7bc
MB7 <beknazarmb@mail.ru>
2021-04-12 16:56:41 +0600

    Написал функцию для регистрации

--- source/accounts/views.py
+++ source/accounts/views.py
@@ -1,23 +1,19 @@
 from django.shortcuts import render, redirect
 from django.contrib.auth import authenticate, login, logout
+from .forms import MyUserCreationForm
 
 
 # Create your views here.
 
-def login_view(request):
-    context = {}
+
+def register_view(request, *args, **kwargs):
     if request.method == 'POST':
-        username = request.POST.get('username')
-        password = request.POST.get('password')
-        user = authenticate(request, username=username, password=password)
-        if user is not None:
+        form = MyUserCreationForm(data=request.POST)
+        if form.is_valid():
+            user = form.save()
             login(request, user)
-            return redirect('index_view')
-        else:
-            context['has_error'] = True
-    return render(request, 'registration/login.html', context=context)
+            return redirect('task:view')
+    else:
+        form = MyUserCreationForm()
+    return render(request, 'registration/regis.html', context={'form': form})
 
-
-def logout_view(request):
-    logout(request)
-    return redirect('index_view')
