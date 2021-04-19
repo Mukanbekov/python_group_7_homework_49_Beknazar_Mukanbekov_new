ca726c4012c3f2b4f888725fd9b7146b72093f8c
MB7 <beknazarmb@mail.ru>
2021-04-19 18:32:38 +0600

    Добавил модель

--- source/accounts/models.py
+++ source/accounts/models.py
@@ -1,3 +1,16 @@
 from django.db import models
+from django.contrib.auth import get_user_model
 
-# Create your models here.
+
+class Profile(models.Model):
+    user = models.OneToOneField(get_user_model(), related_name='profile', on_delete=models.CASCADE, verbose_name='Пользователь')
+    avatar = models.ImageField(null=True, blank=True, upload_to='user_pics', verbose_name='Аватар')
+    git = models.URLField(max_length=70, blank=True, null=True, verbose_name='Ссылка')
+    description = models.TextField(max_length=500, blank=True, null=True, verbose_name='Поле')
+
+    class Meta:
+        verbose_name = 'Профиль'
+        verbose_name_plural = 'Профили'
+        permissions =[
+            ( 'You_have_no_rights','Проверка профиля')
+        ]
